83a12016ac59f7f0575a531c6ab1f9669ef3453b
Muhsin Ünsal <zorkon2002@gmail.com>
2024-05-31 23:00:10 +0300

    Migrated to TypeScript , rewroted communication

--- data/script/script.js
+++ data/script/script.js
@@ -1,197 +1,139 @@
-class Package {
-    constructor(type, pack = null) {
-        this.type = type;
-        this.package = pack;
-    }
-}
-
-class ColorPackage extends Package {
-    constructor(red = 189, green = 189, blue = 189, alpha = 189, hue = 189, saturation = 189, value = 189) {
-        super("color");
-        let rgba = { r: red, g: green, b: blue, a: alpha };
-        let hsv = { h: hue, s: saturation, v: value };
-        this.package = { color: { "rgba": rgba, "hsv": hsv } };
-    }
-}
-
-class PatternIndexPackage extends Package {
-    constructor(patternIndex) {
-        super("patternIndex");
-        this.package = { "patternIndex": patternIndex }
-    }
-}
-
-class PaletteIndexPackage extends Package {
-    constructor(paletteIndex) {
-        super("paletteIndex");
-        this.package = { "paletteIndex": paletteIndex }
-    }
-}
-
-
-let colorPicker_Element = document.getElementsByClassName("IroWheel")[0];
-
-let patternDropdown_Element = document.getElementById("pattern_selector");
-let paletteDropdown_Element = document.getElementById("palette_selector");
-
-let patternOptions = [];
-let paletteOptions = [];
-
-let oldColorPackage = new ColorPackage();
-
-let own_ID;
-let lastMessageOwner_ID = 0; // ID
-
-let socket_status_symbol = document.getElementById(
-    "socket_status_symbol"
-);
-socket = new WebSocket("ws:/" + "/" + "192.168.1.215" + ":81");
-socket.onopen = function (e) {
+var colorPicker_Element = document.getElementsByClassName("IroWheel")[0];
+var patternDropdown_Element = document.getElementById("pattern_selector");
+var paletteDropdown_Element = document.getElementById("palette_selector");
+var patternOptions = [];
+var paletteOptions = [];
+var lastUpdateMessage;
+var own_ID;
+var lastMessageOwner_ID = 0; //ID
+var socket_status_symbol = document.getElementById("socket_status_symbol");
+var webSocket = new WebSocket("ws:/" + "/" + "192.168.1.215" + ":81");
+webSocket.onopen = function (e) {
     socket_status_symbol.style.backgroundColor = "#00cc00"; // #88534D
     socket_status_symbol.innerHTML = "✓";
-    console.log("[socket] WebSocket attatched. ");
-
-
-    colorPicker.on("color:change", (color) => {
-        const newColorVar = {
-            rgba: color.rgba,
-            hsv: color.hsv
-        };
-
-        //console.table(newColorVar)
-
-        if (socket.readyState === socket.OPEN) {
-            if (JSON.stringify(newColorVar) != JSON.stringify(oldColorPackage.package.color)) {
-
-                oldColorPackage.package.color = newColorVar
-                socket.send(JSON.stringify(oldColorPackage));
-                //console.table(oldColorPackage.package.color)
+    console.log("%c[Socket Attatched]", "font-weight: bold;font-size:1rem");
+    colorPicker.on("color:change", function (newColor) {
+        // 		//console.table(newColorVar)
+        if (webSocket.readyState === webSocket.OPEN) {
+            if (JSON.stringify(newColor.rgba) != JSON.stringify(lastUpdateMessage.rgba)) {
+                lastUpdateMessage.rgba = newColor.rgba;
+                webSocket.send(JSON.stringify(lastUpdateMessage));
+                console.log("%c[Socket Sent]: ", "font-weight: bold;font-size:1rem", lastUpdateMessage);
             }
-        } else {
+        }
+        else {
             socket_status_symbol.style.backgroundColor = "#800000"; // #88534D
             socket_status_symbol.innerHTML = "✖";
-            document.querySelector(".desktop-error").classList.add("showen")
-            document.querySelector(".mobile-error").classList.add("showen")
-
+            document.querySelector(".desktop-error").classList.add("shown");
+            document.querySelector(".mobile-error").classList.add("shown");
         }
     });
 };
-socket.onerror = function (e) {
-    console.log("[socket] WebSocket had an Error. ");
+webSocket.onerror = function (e) {
+    console.log("%c[Socket Error]", "font-weight: bold;font-size:1rem");
 };
-socket.onmessage = function (e) {
-    console.log("[SOCKET RESPONSE] " + e.data);
-    let ignoreFlag = false;
-
-    const json_message = JSON.parse(e.data)
-
-    switch (json_message.type) {
-
-        //One time at start message
-        case "startMessage":
-
-            // Assign number to client
-            own_ID = json_message.userID;
-            const idElement = document.getElementById("clientID");
-            idElement.innerHTML = `You're ${own_ID + 1}st user`;
-
-            // Clear Options
-            patternDropdown_Element.innerHTML = "";
-            paletteDropdown_Element.innerHTML = "";
-            // Fill up Pattern Options
-            for (let i = 0; i < json_message.package.pattern_list.length; i++) {
-                const element = document.createElement("option");
-                element.setAttribute("value", i);
-                element.innerText = json_message.package.pattern_list[i];
-
-                patternDropdown_Element.appendChild(element)
-            }
-
-            // Fill up Palette Options
-            for (let i = 0; i < json_message.package.palette_list.length; i++) {
-                const element = document.createElement("option");
-                element.setAttribute("value", i);
-                element.innerText = json_message.package.palette_list[i];
-
-                paletteDropdown_Element.appendChild(element)
-            }
-
-            break;
-
-        case "color":
-            // Setting ignore to pervent echoing
-            /////////////// DÜZEEEELLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLT
-            ignoreFlag = true;
-            setTimeout(() => {
-                console.log("IgnoreFlag", ignoreFlag)
-                ignoreFlag = false;
-            }, 100);
-
-            if (ignoreFlag) {
-                let rgba = json_message.package.rgba
-                colorPicker.color.rgba = rgba
-            }
-            break;
-        case "patternIndex":
-            patternDropdown_Element.value = json_message.package.patternIndex;
-            break;
-
-        case "paletteIndex":
-            paletteDropdown_Element.value = json_message.package.paletteIndex;
-            break;
+webSocket.onmessage = function (e) {
+    var ignoreFlag = false;
+    var json_message;
+    json_message = JSON.parse(e.data);
+    if (json_message.type == "startMessage") {
+        json_message = json_message;
+        console.log("%c[Socket Recived]: Start Message", "font-weight: bold;font-size:1rem", json_message);
+        // Assign number to client
+        own_ID = json_message.assignedID;
+        document.getElementById("clientID").innerHTML = "You're ".concat(own_ID + 1, "st user");
+        // Clear Options
+        patternDropdown_Element.innerHTML = "";
+        paletteDropdown_Element.innerHTML = "";
+        // Fill up Pattern Options
+        json_message.patternList.forEach(function (pattern, i) {
+            var element = document.createElement("option");
+            element.setAttribute("value", String(i));
+            element.innerText = pattern;
+            patternDropdown_Element.appendChild(element);
+        });
+        // Fill up Palette Options
+        json_message.paletteList.forEach(function (palette, i) {
+            var element = document.createElement("option");
+            element.setAttribute("value", String(i));
+            element.innerText = palette;
+            paletteDropdown_Element.appendChild(element);
+        });
     }
-
+    else if (json_message.type == "updateMessage") {
+        json_message = json_message;
+        console.log("%c[Socket Recived]: Update Message", "font-weight: bold;font-size:1rem", json_message);
+        lastUpdateMessage = json_message;
+        //Color
+        // Setting ignore to pervent echoing
+        // 			/////////////// DÜZEEEELLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLT
+        ignoreFlag = true;
+        setTimeout(function () {
+            console.log("IgnoreFlag:", ignoreFlag);
+            ignoreFlag = false;
+        }, 100);
+        if (ignoreFlag) {
+            var rgba = json_message.rgba;
+            colorPicker.color.rgba = rgba;
+        }
+        patternDropdown_Element.value = String(json_message.patternIndex);
+        paletteDropdown_Element.value = String(json_message.paletteIndex);
+    }
+    else {
+        console.log("%c[Socket Recived]: ", "font-weight: bold;font-size:1rem", e.data);
+    }
     //document.getElementById("change_this").innerHTML = e.data;
 };
-
-patternDropdown_Element.addEventListener("change", (e) => {
-    const package = new PatternIndexPackage(patternDropdown_Element.value);
-    socket.send(JSON.stringify(package));
+patternDropdown_Element.addEventListener("change", function (e) {
+    lastUpdateMessage.patternIndex = Number(patternDropdown_Element.value);
+    webSocket.send(JSON.stringify(lastUpdateMessage));
+    console.log("%c[Socket Sent]: ", "font-weight: bold;font-size:1rem", lastUpdateMessage);
 });
-
-paletteDropdown_Element.addEventListener("change", (e) => {
-    const package = new PaletteIndexPackage(paletteDropdown_Element.value);
-    console.log(package)
-    socket.send(JSON.stringify(package));
+paletteDropdown_Element.addEventListener("change", function (e) {
+    lastUpdateMessage.paletteIndex = Number(paletteDropdown_Element.value);
+    webSocket.send(JSON.stringify(lastUpdateMessage));
+    console.log("%c[Socket Sent]: ", "font-weight: bold;font-size:1rem", lastUpdateMessage);
 });
-
-let sliderDirection;
+var sliderDirection;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 var colorPicker = new iro.ColorPicker("#color_picker", {
     wheelLightness: false,
     margin: 20,
     layout: [
         {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
             component: iro.ui.Wheel,
             options: {
-                borderWidth: 5
-            }
+                borderWidth: 5,
+            },
         },
         {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
             component: iro.ui.Slider,
             options: {
-                sliderType: 'alpha',
-                borderWidth: 2
-
-            }
-
+                sliderType: "alpha",
+                borderWidth: 2,
+            },
         },
     ],
     layoutDirection: sliderDirection,
 });
-
 if (screen.width >= screen.height) {
     sliderDirection = "horizontal";
-} else {
+}
+else {
     sliderDirection = "vertical";
 }
-
 /*
-let power = { element: document.getElementById("power"), status: true };
-power.element.addEventListener("click", () => {
-if (power.status) {
-power.status = !power.status;
-} else {
-power.status = !power.status;
-}
-});
-*/
+ let power = { element: document.getElementById("power"), status: true };
+ power.element.addEventListener("click", () => {
+ if (power.status) {
+ power.status = !power.status;
+ } else {
+ power.status = !power.status;
+ }
+ });
+ */
